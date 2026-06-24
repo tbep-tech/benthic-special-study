818a25884ea5f33f67d05ca56221aae1acf05638
fawda123 <mbafs2012@gmail.com>
2026-06-24 15:46:29 -0400

    2026 added

--- R/2026sites.R
+++ R/2026sites.R
@@ -5,6 +5,9 @@ library(mapedit)
 library(here)
 library(spsurvey)
 library(dplyr)
+library(lwgeom)
+
+# gandy ------------------------------------------------------------------
 
 data(tbsegdetail, package = 'tbeptools')
 
@@ -15,72 +18,118 @@ m <- leaflet() |>
 # https://www.fdotd7studies.com/projects/gandy-4th-to-westshore/final-project-documents/
 # https://www.fdotd7studies.com/projects/wp-content/uploads/sites/7/pdf/441250-1-Gandy-Blvd-PDE-Preferred-Alternative-Concept-Plans.pdf
 
-# draw extent of new span footprint
-poly <- drawFeatures(m)
-gandypolyfull <- poly |> st_make_valid()
-save(gandypolyfull, file = here('data/gandypolyfull.RData'))
+gandyline <- drawFeatures(m)
 
-# remove portion near channel
-load(file = here('data/gandypolyfull.RData'))
-m <- leaflet() |> 
- addProviderTiles('Esri.WorldImagery') |> 
- addPolygons(data = gandypolyfull, weight =  0.5)
+save(gandyline, file = here('data/gandyline.RData'))
 
-tocut <- drawFeatures(m)
+load(file = here('data/gandyline.RData'))
 
-gandypoly <- st_difference(gandypolyfull, tocut) |> 
-  st_cast("POLYGON") |>
-  st_make_valid() |>
-  select(geometry)
-
-save(gandypoly, file = here('data/gandypoly.RData'))
-
-# create random points
-load(file = here('data/gandypoly.RData'))
-
-# get points using st_sample, randomly proportional to area
-
-min_dist <- 400  # in CRS units (feet for EPSG:6443)
-
-candidates <- gandypoly |> 
-  st_transform(crs = 6443) |>
-  st_sample(size = 500, type = "random") |>
-  st_as_sf()
-
-selected <- candidates[1, ]
-for (i in 2:nrow(candidates)) {
-  dists <- st_distance(candidates[i, ], selected)
-  if (all(as.numeric(dists) > min_dist)) {
-    selected <- rbind(selected, candidates[i, ])
-  }
-  if (nrow(selected) >= 15) break
-}
-
-gandypts <- selected |>
+gandypts <- st_line_sample(st_transform(gandyline, crs = 6443), n = 10, type = "regular") |>
+  st_as_sf() |>
+  st_cast("POINT") |>
   mutate(
-    siteID = row_number(),
-    backup = sample(c(rep(TRUE, 5), rep(FALSE, 10)))
+    backup = FALSE
   ) |> 
   st_transform(crs = 4326)
 
+gandyptsback <- st_line_sample(st_transform(gandyline, crs = 6443), n = 5, type = "regular") |>
+  st_as_sf() |>
+  st_cast("POINT") |>
+  mutate(
+    backup = TRUE
+  ) |> 
+  st_transform(crs = 4326)
+
+gandypts <- rbind(gandypts, gandyptsback)
+
 leaflet(gandypts) |>
-  addProviderTiles('Esri.WorldImagery') |>
-  addPolygons(data = gandypoly, weight =  0.5) |>
+  addProviderTiles('CartoDB.Positron') |>
+  addPolylines(data = gandyline, weight =  0.5) |>
   addCircleMarkers(
     color = ~ifelse(backup, "red", "blue"),
     radius = 4,
     stroke = FALSE,
-    fillOpacity = 0.8,
-    label = ~siteID
+    fillOpacity = 0.8
   )
 
-tosv <- gandypts |>
-  st_drop_geometry() |>
-  select(siteID, backup) |>
-  mutate(
-    Y = st_coordinates(gandypts)[, 2],
-    X = st_coordinates(gandypts)[, 1]
-  ) |> 
-  select(X, Y, backup, siteID)
+# hillsborough -----------------------------------------------------------
 
-write.csv(tosv, file = here('data/gandypts.csv'), row.names = FALSE)
+data(tbsegdetail, package = 'tbeptools')
+
+m <- leaflet() |> 
+ addProviderTiles('CartoDB.Positron') |> 
+ addPolygons(data = tbsegdetail, weight =  0.5)
+
+hillline <- drawFeatures(m)
+save(hillline, file = here('data/hillline.RData'))
+
+load(file = here('data/hillline.RData'))
+
+hillpts <- st_line_sample(st_transform(hillline, crs = 6443), n = 10, type = "regular") |>
+  st_as_sf() |>
+  st_cast("POINT") |>
+  mutate(
+    backup = FALSE
+  ) |> 
+  st_transform(crs = 4326)
+
+hillptsback <- st_line_sample(st_transform(hillline, crs = 6443), n = 5, type = "regular") |>
+  st_as_sf() |>
+  st_cast("POINT") |>
+  mutate(
+    backup = TRUE
+  ) |> 
+  st_transform(crs = 4326)
+
+hillpts <- rbind(hillpts, hillptsback)
+
+leaflet(hillpts) |>
+  addProviderTiles('CartoDB.Positron') |>
+  addPolylines(data = hillline, weight =  0.5) |>
+  addCircleMarkers(
+    color = ~ifelse(backup, "red", "blue"),
+    radius = 4,
+    stroke = FALSE,
+    fillOpacity = 0.8
+  )
+
+# save both as csv -------------------------------------------------------
+
+gandypts <- gandypts |>
+  mutate(
+    site = 'Gandy'
+  )
+hillpts <- hillpts |>
+  mutate(
+    site = 'Hillsborough'
+  )
+
+pts <- rbind(gandypts, hillpts) |> 
+  mutate(
+    siteID = row_number()
+  )
+
+tosv <- pts |>
+  st_drop_geometry() |>
+  select(site, siteID, backup) |>
+  mutate(
+    Y = st_coordinates(pts)[, 2],
+    X = st_coordinates(pts)[, 1]
+  ) |> 
+  select(X, Y, backup, site, siteID)
+
+write.csv(tosv, file = here('data/pts2026.csv'), row.names = FALSE)
+
+# verify
+pts <- read.csv(here('data/pts2026.csv')) |>
+  st_as_sf(coords = c("X", "Y"), crs = 4326)
+
+leaflet(pts) |>
+  addProviderTiles('CartoDB.Positron') |>
+  addCircleMarkers(
+    color = ~ifelse(backup, "red", "blue"),
+    radius = 4,
+    stroke = FALSE,
+    fillOpacity = 0.8, 
+    label = ~paste0(site, " - ", siteID)
+  )
